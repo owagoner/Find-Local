652f144002340804d5c1800fd58d7e9e39c3b86d
owagoner <owagoner@DESKTOP-NVS6AMB>
2017-02-27 08:29:45 -0500

    Add project files.

---
+++ FindLocal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindLocal.Startup))]
+namespace FindLocal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
